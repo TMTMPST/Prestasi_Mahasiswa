77ba7a06a508a4e352f83409da8de34631e94e0e
Vidi Joshubzky Saviola <103508462+TMTMPST@users.noreply.github.com>
2024-12-10 15:13:31 +0700

    asdasd

---
+++ Trying/component/navbar.php
@@ -0,0 +1,16 @@
+<?php
+function renderNavbar() {
+    ?>
+    <nav class="top-nav">
+        <button class="menu-toggle">☰</button>
+        <div class="user-menu">
+            <img src="../img/dummy/1.jpg" alt="User Avatar" class="user-avatar">
+            <div class="user-dropdown">
+                <a href="editProfile.php">Profile</a>
+                <a href="../login/login.php">Logout</a>
+            </div>
+        </div>
+    </nav>
+    <?php
+}
+?>
